478bbfd7032189c81dcfaea09910e05a14d40cd2
xXelyazXx <eliasojeda4@gmail.com>
2024-09-09 16:00:22 -0300

    collapse añadido en header

--- app/components/header.js
+++ app/components/header.js
@@ -1,8 +1,6 @@
 "use client"
-import Link from "next/link";
-import React from "react";
-import { Image, Box } from '@chakra-ui/react';
-
+import { useState } from 'react';
+import { Box, Image, Link, Collapse, useDisclosure, useMediaQuery, Button } from '@chakra-ui/react';
 
 const links = [
   { nombre: "Acerca de nosotros", url: "/acerca" },
@@ -10,8 +8,12 @@ const links = [
   { nombre: "Donde encontrarnos", url: "/ubicaciones" },
 ];
 
-const tamanoImagen = "70px"
+const tamanoImagen = "70px";
+
 export default function Header() {
+  const { isOpen, onToggle, onClose } = useDisclosure();
+  const [isSmallScreen] = useMediaQuery("(max-width: 600px)");
+
   return (
     <nav className="navegacion">
       <Box width={tamanoImagen} height={tamanoImagen}>
@@ -19,13 +21,47 @@ export default function Header() {
           <Image src="/imagenes/images-removebg-preview.png" alt="logo" boxSize={tamanoImagen} />
         </Link>
       </Box>
-      {links.map((link) => (
-        <Box>
-          <Link href={link.url} key={link.url}>
-            {link.nombre}
-          </Link>
+      {isSmallScreen ? (
+        <>
+          <Button onClick={onToggle}>Menu</Button>
+          <Collapse in={isOpen} animateOpacity>
+            <Box
+              position="absolute"
+              top="0"
+              left="0"
+              width="100%"
+              height="100vh"
+              bg="white"
+              zIndex="10"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Button onClick={onClose} position="absolute" top="10px" right="10px">
+                Cerrar
+              </Button>
+              {links.map((link) => (
+                <Box key={link.url} margin="10px 0">
+                  <Link href={link.url}>
+                    {link.nombre}
+                  </Link>
+                </Box>
+              ))}
+            </Box>
+          </Collapse>
+        </>
+      ) : (
+        <Box display="flex" flexDirection="row" alignItems="center">
+          {links.map((link) => (
+            <Box key={link.url} margin="10px">
+              <Link href={link.url}>
+                {link.nombre}
+              </Link>
+            </Box>
+          ))}
         </Box>
-      ))}
+      )}
     </nav>
   );
 }
