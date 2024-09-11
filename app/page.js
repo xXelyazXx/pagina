eb582d202a07952ad79951f07c88dca0150a4e63
xXelyazXx <eliasojeda4@gmail.com>
2024-09-11 18:14:52 -0300

    video pagina de inicio

--- app/page.js
+++ app/page.js
@@ -2,24 +2,26 @@ export default function Home() {
   return (
     <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }}>
       <video
-        className="responsive-video"
-        loop
-        muted
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          transform: 'translate(-50%, -50%)',
-        }}
-      >
-        Your browser does not support the video tag.
-      </video>
-      <div>
-        Pagina de inicio
-      </div>
+  className="responsive-video"
+  loop
+  muted
+  autoPlay
+  style={{
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    transform: 'translate(-50%, -50%)',
+  }}
+>
+<source src="/videos/Bienvenido.mp4" type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
+<div>
+  Pagina de inicio
+</div>
     </div>
   );
 }
