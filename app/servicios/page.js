ebd42b520c0d7cbac8a407244044208364d69a9f
Elias Ignacio Ojeda Mercado <eliasojeda4@gmail.com>
2025-04-13 19:59:20 -0400

    horarios servicios

--- app/servicios/page.js
+++ app/servicios/page.js
@@ -1,7 +1,14 @@
+"use client";
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+
+
 export default function Page() {
-    return(
+    return (
         <div>
-            hola mundo
+            <Box maxW="90%" mx="auto" mt={5} borderRadius="lg" overflow="hidden">
+                <Image src="paisaje2.jpeg" alt="paisaje1" objectFit="cover" />
+            </Box>
         </div>
     )
 }
