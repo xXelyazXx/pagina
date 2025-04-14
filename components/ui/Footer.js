6a5043ff1bb11d69242eedc28f83ff6f66e92db6
Elias Ignacio Ojeda Mercado <eliasojeda4@gmail.com>
2025-04-14 15:18:45 -0400

    informacion footer

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -1,9 +1,47 @@
-import { Box, Text } from "@chakra-ui/react";
+"use client"
+import { Box, Flex, Stack, Text, Link, Icon, Divider, SimpleGrid } from "@chakra-ui/react";
+import { FaInstagram, FaYoutube, FaPhone, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Box as="footer" bg="black" color="white" p={4} mt={8}>
-      <Text>&copy; 2025 My Website. All rights reserved.</Text>
+    <Box bg="gray.800" color="white" px={6} py={10} >
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+        {/* Contacto */}
+        <Stack spacing={3}>
+          <Text fontSize="lg" fontWeight="bold">Contacto</Text>
+          <Flex align="center"><Icon as={FaPhone} mr={2} /> +56 9 1234 5678</Flex>
+          <Flex align="center"><Icon as={FaEnvelope} mr={2} /> contacto@fvn.cl</Flex>
+        </Stack>
+
+        {/* Redes Sociales */}
+        <Stack spacing={3}>
+          <Text fontSize="lg" fontWeight="bold">Redes Sociales</Text>
+          <Link href="https://instagram.com/fvniglesia" isExternal target="_blank" rel="noopener noreferrer">
+            <Flex align="center"><Icon as={FaInstagram} mr={2} /> @fvniglesia</Flex>
+          </Link>
+          <Link href="https://www.youtube.com/@FVNIglesia/" isExternal target="_blank" rel="noopener noreferrer">
+            <Flex align="center"><Icon as={FaYoutube} mr={2} /> FVN iglesia </Flex>
+          </Link>
+        </Stack>
+
+        {/* Ubicación y Horarios */}
+        <Stack spacing={3}>
+          <Text fontSize="lg" fontWeight="bold">Dónde encontrarnos</Text>
+          <Link href="https://maps.app.goo.gl/ox1q4PmWwSCKskn9A" isExternal target="_blank" rel="noopener noreferrer"> 
+          <Flex align="center"><Icon as={FaMapMarkerAlt} mr={2} /> Calle Ficticia 123, Valdivia</Flex>
+          </Link>
+          <Text>Horario de atención: Lunes a Viernes, 10:00 - 18:00 hrs</Text>
+        </Stack>
+
+        {/* Grupos y tiempos */}
+        <Stack spacing={3}>
+          <Text fontSize="lg" fontWeight="bold">Grupos de vida</Text>
+          <Text>GDV Mujeres: Miercoles</Text>
+          <Text>GDV Hombres: Jueves</Text>
+          <Text>GDV Jóvenes: Viernes </Text>
+          <Text>GDV Tweens/youth: Sábados</Text>
+        </Stack>
+      </SimpleGrid>
     </Box>
   );
 };
